2e2748378c7cfd5a27bdea56632c1fe5a6d1c06a
anna-vaughn <60945664+anna-vaughn@users.noreply.github.com>
2026-05-03 19:37:00 -0500

    add graph of loss values

---
+++ loss_graph.py
@@ -0,0 +1,16 @@
+# Output from running train.py (taken from my console).
+## Epoch  1/3  Train Loss: 0.3445  Val Loss: 0.3077
+## Epoch  2/3  Train Loss: 0.2226  Val Loss: 0.3116
+## Epoch  3/3  Train Loss: 0.1698  Val Loss: 0.1518
+
+import matplotlib.pyplot as plt
+
+train_loss = [0.3445, 0.2226, 0.1698]
+val_loss = [0.3077, 0.3116, 0.1518]
+epochs = [1,2,3]
+
+plt.plot(epochs, train_loss, label='Train Loss')
+plt.plot(epochs, val_loss, label='Val Loss')
+plt.legend()
+plt.savefig("Output/loss_graph.png")
+plt.show()
